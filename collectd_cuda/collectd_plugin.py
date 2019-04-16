88434776d343f63ec7678d4e2a2b1f1bca440e85
auxlife <theju@auxlife.com>
2019-04-16 13:20:11 -0700

    Update collectd_plugin.py
    Changed check function to normalize from isvalid.

--- collectd_cuda/collectd_plugin.py
+++ collectd_cuda/collectd_plugin.py
@@ -10,7 +10,7 @@ def configure(conf):
         collectd.info('Configured with')
 
 # Checks for N/A or ERR! readings and returns 0.0
-def isvalid(reading):
+def normalize(reading):
         if reading == "N/A" or reading == "ERR!":
                 return 0.0
         else:
@@ -31,34 +31,34 @@ def read(data=None):
 
                 # GPU utilization
                 vl.dispatch(type='percent', type_instance='gpu_util',
-                            values=[isvalid(gpu.find('utilization/gpu_util').text.split()[0])])
+                            values=[normalize(gpu.find('utilization/gpu_util').text.split()[0])])
                 # GPU temperature
                 vl.dispatch(type='temperature',
-                            values=[isvalid(gpu.find('temperature/gpu_temp').text.split()[0])])
+                            values=[normalize(gpu.find('temperature/gpu_temp').text.split()[0])])
                 # GPU power draw
                 vl.dispatch(type='power', type_instance='power_draw',
-                            values=[isvalid(gpu.find('power_readings/power_draw').text.split()[0])])
+                            values=[normalize(gpu.find('power_readings/power_draw').text.split()[0])])
                 # GPU memory utilization
                 vl.dispatch(type='percent', type_instance='mem_util',
-                            values=[isvalid(gpu.find('utilization/memory_util').text.split()[0])])
+                            values=[normalize(gpu.find('utilization/memory_util').text.split()[0])])
                 # GPU encoder utilization
                 vl.dispatch(type='percent', type_instance='enc_util',
-                            values=[isvalid(gpu.find('utilization/encoder_util').text.split()[0])])
+                            values=[normalize(gpu.find('utilization/encoder_util').text.split()[0])])
                 # GPU decoder utilization
                 vl.dispatch(type='percent', type_instance='dec_util',
-                            values=[isvalid(gpu.find('utilization/decoder_util').text.split()[0])])
+                            values=[normalize(gpu.find('utilization/decoder_util').text.split()[0])])
                 # GPU memory usage
                 vl.dispatch(type='memory', type_instance='used',
-                            values=[1e6 * isvalid(gpu.find('fb_memory_usage/used').text.split()[0])])
+                            values=[1e6 * normalize(gpu.find('fb_memory_usage/used').text.split()[0])])
                 # GPU total memory
                 vl.dispatch(type='memory', type_instance='total',
-                            values=[1e6 * isvalid(gpu.find('fb_memory_usage/total').text.split()[0])])
+                            values=[1e6 * normalize(gpu.find('fb_memory_usage/total').text.split()[0])])
                 # GPU frequency
                 vl.dispatch(type='cpufreq', type_instance='gpu_clock',
-                            values=[1e6 * isvalid(gpu.find('clocks/graphics_clock').text.split()[0])])
+                            values=[1e6 * normalize(gpu.find('clocks/graphics_clock').text.split()[0])])
                 # GPU memory frequency
                 vl.dispatch(type='cpufreq', type_instance='mem_clock',
-                            values=[1e6 * isvalid(gpu.find('clocks/mem_clock').text.split()[0])])
+                            values=[1e6 * normalize(gpu.find('clocks/mem_clock').text.split()[0])])
 
 collectd.register_config(configure)
 collectd.register_read(read)
